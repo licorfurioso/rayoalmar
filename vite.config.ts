0acb3f53befa379296d39898f241bf325ecea8c6
licorfurioso <florenciadelcguzman@gmail.com>
2025-12-01 16:18:24 +0100

    Iteration 6: remove base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/rayoalmar/',
+  base: '/',
   build: {
     // Optimize production build
     minify: 'esbuild',
